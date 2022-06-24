c4591346c176fecfb7983cd11055d5b0784be788
Mahmoud Nasr <mmmnnn2016161515@gmail.com>
2022-06-25 01:07:36 +0200

    permssion

---
+++ resources/views/errors/403.blade.php
@@ -0,0 +1,12 @@
+@extends('dashboard.layouts.index')
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-12 " style="text-align: center">
+                <img src="{{ asset('images/stop.png') }}"style="width: 300px;margin-top: 90px">
+            </div>
+        </div>
+    </div>
+
+    <h2 style="text-align: center">ليس لديك صلاحية الوصول الي هذه الصفحة</h2>
+@stop
